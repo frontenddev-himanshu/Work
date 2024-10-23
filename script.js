8172cfe3a8ed3840ba7067ec9941e1321adc038d
frontenddev-himanshu <rajh5343@gmail.com>
2024-10-23 18:57:16 +0530

    final touch

--- script.js
+++ script.js
@@ -19,3 +19,23 @@ hamb.addEventListener("click",function(){
   
 
 })      
+
+
+
+// swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay : {
+    delay : 1700,
+  }
+});
